7ff277e2f29e797a92b367034fcd57d3c9eaba61
Aleksandra <olavesely@gmail.com>
2024-10-02 21:19:29 +0200

    Add require method in views and change paths in urls to be in the same convention for every endpoint

--- pet_adoption_project/pet_adoption_app/views.py
+++ pet_adoption_project/pet_adoption_app/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
 
 from .forms import RegisterForm, PetInDatabase
 from .models import Pet, Question
@@ -17,10 +18,12 @@ PARAMETERS = ['size', 'age', 'sex', 'breed', 'temperament', 'personality', 'kid_
               'cat_friendly', 'neutered']
 
 
+@require_http_methods(["GET"])
 def home(request):
     return render(request, 'home.html', {})
 
 
+@require_http_methods(["GET"])
 def pets(request):
     all_pets = Pet.objects.all()
     return render(request, 'pets.html', {
@@ -28,6 +31,7 @@ def pets(request):
     })
 
 
+@require_http_methods(["GET"])
 def quiz(request):
     question_objs = Question.objects.all()
     return render(request, 'quiz_main_page.html', {
@@ -39,6 +43,7 @@ class TooFewAnswers(Exception):
     pass
 
 
+@require_http_methods(["GET"])
 def submit_form(request):
     responses = QuizManager().prepare_a_list_of_responses(request)
 
@@ -56,6 +61,7 @@ def submit_form(request):
         raise Http404("Too few answers were given.")
 
 
+@require_http_methods(["GET"])
 def pet_detail(request, pet_id):
     try:
         pet = Pet.objects.get(id=pet_id)
@@ -64,6 +70,7 @@ def pet_detail(request, pet_id):
     return render(request, 'pet_detail.html', {'pet': pet})
 
 
+@require_http_methods(["POST", "GET"])
 def sign_up(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -77,12 +84,14 @@ def sign_up(request):
     return render(request, 'registration/sign_up.html/', {'form': form})
 
 
+@require_http_methods(["GET"])
 @user_passes_test(lambda u: u.groups.filter(name='Moderators').exists())
 def moderator_dashboard(request):
     database_table = Pet.objects.all()
     return render(request, 'moderator_dashboard.html', {'database_table': database_table})
 
 
+@require_http_methods(["POST", "GET"])
 def edit(request, pet_id):
     pet = get_object_or_404(Pet, pk=pet_id)
     if request.method == "POST":
@@ -96,6 +105,7 @@ def edit(request, pet_id):
     return render(request, 'database_actions/edit.html', {'formset': formset})
 
 
+@require_http_methods(["POST", "GET"])
 def create_a_record(request):
     if request.method == "POST":
         formset = PetInDatabase(request.POST)
